4eefa69aabaf0afd6807ca71f2d2241d6f196484
Fabricio <fabri_mu@hotmail.com>
2019-03-25 17:01:59 -0300

    adicionando css

--- tupperTools/settings.py
+++ tupperTools/settings.py
@@ -133,11 +133,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    'statics',
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'statics'),
+)
 # Local para arquivos upado
 MEDIA_URL = '/files/'
 MEDIA_ROOT = 'files'
