910bdce9c17b40c9afc054b71fa6061d48ae0549
Fabricio <fabri_mu@hotmail.com>
2019-03-26 16:05:04 -0300

    Teste CSS heroku

--- tupperTools/settings.py
+++ tupperTools/settings.py
@@ -133,13 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 # Local para arquivos upado
 MEDIA_URL = '/files/'
 MEDIA_ROOT = 'files'
