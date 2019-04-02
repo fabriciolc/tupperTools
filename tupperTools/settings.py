882965223336b52099a862ecead026b069620d05
Fabricio <fabri_mu@hotmail.com>
2019-04-02 18:18:54 -0300

    fix cors

--- tupperTools/settings.py
+++ tupperTools/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['tupper-tools.herokuapp.com','localhost', '192.168.10.54','localhost:8080','*']
+ALLOWED_HOSTS = ['tupper-tools.herokuapp.com','localhost','*']
 
 
 # Application definition
@@ -164,4 +164,4 @@ LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'home'
 
-CORS_ORIGIN_WHITELIST = 'localhost:8000','localhost:8100','*'
+CORS_ORIGIN_WHITELIST = 'localhost:8000','localhost:8100','localhost:8080'
